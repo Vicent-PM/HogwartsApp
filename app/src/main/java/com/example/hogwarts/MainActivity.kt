821b9cbbe0350c95ad48c81a76f9c559b36c59dd
Vicent-PM <114580711+Vicent-PM@users.noreply.github.com>
2023-05-30 13:09:07 +0200

    Terminado Menu Lateral

--- app/src/main/java/com/example/hogwarts/MainActivity.kt
+++ app/src/main/java/com/example/hogwarts/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -37,28 +37,28 @@ class MainActivity : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.characterFragment, R.id.housesFragment, R.id.spellsFragment
+                R.id.FirstFragment, R.id.characterFragment, R.id.housesFragment, R.id.spellsFragment
             ), binding.drawerlayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navigationview.setupWithNavController(navController)
 
-//        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-//            override fun handleOnBackPressed() {
-//                if(binding.drawerlayout.isDrawerOpen(GravityCompat.START)) {
-//                    binding.drawerlayout.closeDrawer(GravityCompat.START)
-//                } else {
-//                    if(!navController.navigateUp())
-//                        finish()
-//                }
-//            }
-//        })
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if(binding.drawerlayout.isDrawerOpen(GravityCompat.START)) {
+                    binding.drawerlayout.closeDrawer(GravityCompat.START)
+                } else {
+                    if(!navController.navigateUp())
+                        finish()
+                }
+            }
+        })
 
         val navigationView: NavigationView = findViewById(R.id.navigationview)
         val headerView = navigationView.getHeaderView(0)
         val usuario_header = headerView.findViewById<TextView>(R.id.usuario_header)
 
-        usuario_header.text = "Muggle"
+        usuario_header.text = "Pure Blood"
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
