0dcd856a4c095f7b924bff25676e0efcdac2e3cb
Vicent-PM <114580711+Vicent-PM@users.noreply.github.com>
2023-05-31 14:15:38 +0200

    Arreglado Holder, Prueba pasar nombre tabItem a fragment

--- app/src/main/java/com/example/hogwarts/data/adapters/characterViewPagerAdapter.kt
+++ app/src/main/java/com/example/hogwarts/data/adapters/characterViewPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.hogwarts.data.adapters
 
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.hogwarts.ui.fragments.CharacterFragment
@@ -8,11 +9,18 @@ import com.example.hogwarts.ui.fragments.characters.StaffCharactersFragment
 import com.example.hogwarts.ui.fragments.characters.StudentsCharacterFragment
 
 class characterViewPagerAdapter(activity: CharacterFragment) : FragmentStateAdapter(activity) {
+
+//    private val listaCasas = ArrayList<String>()
     override fun getItemCount(): Int {
         return 3
     }
 
     override fun createFragment(position: Int): Fragment {
+
+//        val casa = listaCasas[position]
+//        val bundle = bundleOf("casa" to casa)
+//        val fragment = AllCharactersFragment()
+//        fragment.arguments = bundle
         return if(position == 0) AllCharactersFragment() else if(position == 1) StaffCharactersFragment() else StudentsCharacterFragment()
     }
 }
