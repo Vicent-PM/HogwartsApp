f41738c878ae8a96f7a78d50a6d5e14d33e9e168
Vicent-PM <114580711+Vicent-PM@users.noreply.github.com>
2023-05-31 11:39:21 +0200

    Detalle personaje a medias

--- app/src/main/java/com/example/hogwarts/ui/fragments/CharacterDetailsFragment.kt
+++ app/src/main/java/com/example/hogwarts/ui/fragments/CharacterDetailsFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
+import com.example.hogwarts.R
 import com.example.hogwarts.databinding.FragmentDetailsCharcactersBinding
+import com.example.hogwarts.ui.MainActivity
 import com.example.hogwarts.ui.MyViewModel
 
 class CharacterDetailsFragment: Fragment() {
@@ -32,7 +34,49 @@ class CharacterDetailsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
+        myViewModel.selectedCharacter.observe(viewLifecycleOwner) {
+            if(it.hogwartsStudent == true) {
+                (requireActivity() as MainActivity).supportActionBar?.title = "Student"
+            } else if (it.hogwartsStaff == true) {
+                (requireActivity() as MainActivity).supportActionBar?.title = "Staff"
+            } else if(it.wizard == true) {
+                (requireActivity() as MainActivity).supportActionBar?.title = "Wizard"
+            } else {
+                (requireActivity() as MainActivity).supportActionBar?.title = "Muggle"
+            }
+            Glide.with(this).load(it.image).into(binding.tvImagenPersonaje)
+            binding.tvSpecies.text = it.species
+            when(it.gender){
+                "male" -> binding.ivGender.setImageResource(R.drawable.genero_masculino)
+                "female" -> binding.ivGender.setImageResource(R.drawable.femenino)
+            }
+            binding.tvHouse.text = it.house
+            binding.tvDateBirth.text = it.dateOfBirth
+            binding.tvYearBirth.text = it.yearOfBirth.toString()
+            binding.tvWizard.text = it.wizard.toString()
+            binding.tvAncestry.text = it.ancestry
+            binding.tvEyeColour.text = it.eyeColour
+            binding.tvHairColour.text = it.hairColour
+            binding.tvWand.text = it.wand?.core + ", " + it.wand?.wood + ", " + it.wand?.length.toString()
+            binding.tvPatronus.text = it.patronus
+            if(it.hogwartsStudent == true) {
+                binding.ivStundent.setImageResource(R.drawable.baseline_check_24)
+            } else {
+                binding.ivStundent.setImageResource(R.drawable.baseline_clear_24)
+            }
+            if(it.hogwartsStaff == true) {
+                binding.ivStaff.setImageResource(R.drawable.baseline_check_24)
+            } else {
+                binding.ivStaff.setImageResource(R.drawable.baseline_clear_24)
+            }
+            binding.tvActor.text = it.actor
+            if(it.alive == true) {
+                binding.ivAlive.setImageResource(R.drawable.baseline_check_24)
+            } else {
+                binding.ivAlive.setImageResource(R.drawable.baseline_clear_24)
+            }
+        }
     }
 
     override fun onDestroyView() {
